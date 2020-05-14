350a0d5f59732acae9b5fc2cd9d489afac8caf66
Stefano Fioravanzo <stefano@arrikto.com>
2020-05-14 16:31:51 +0200

    backend: Add RPC to get file's owner volume
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

--- backend/kale/utils/pod_utils.py
+++ backend/kale/utils/pod_utils.py
@@ -156,6 +156,25 @@ def list_volumes():
     return _list_volumes(client, namespace, pod_name, container_name)
 
 
+def get_volume_containing_path(path):
+    """Get the closest volume mount point to the input absolute path"""
+    if not os.path.isabs(path):
+        raise ValueError("Input path is not an absolute path")
+    if not os.path.exists(path):
+        raise ValueError("Input path does not exist")
+
+    mounted_vols = list_volumes()
+    mount_point = 0
+    # get the volumes that contain the input path
+    vols = list(filter(lambda x: path.startswith(x[mount_point]),
+                       mounted_vols))
+    if len(vols) > 0:
+        # get vol with longest mount point (i.e. closest to input path)
+        return sorted(vols, key=lambda k: len(k[mount_point]), reverse=True)[0]
+    else:
+        raise RuntimeError("Input path is not under any volume mount point")
+
+
 def get_docker_base_image():
     """Get the current container's docker image."""
     client = _get_k8s_v1_client()
